c91428484c8da45dfe39663562d4c7dcbb035cb7
gtcore902 <gaetan.tremois@gmail.com>
2023-11-29 12:11:00 +0100

    feat: add all items whith for

--- pieces.js
+++ pieces.js
@@ -1,31 +1,33 @@
 // Récupération des pièces depuis le fichier JSON
 const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
-// Création des balises 
-const article = pieces[0];
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
-
-//Création des élements pour l'exercice
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
-
-const stockElement = document.createElement("p");
-stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
-
-//Rattachement de nos balises au DOM
 const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
 
-//Ajout des éléments au DOM pour l'exercice
-sectionFiches.appendChild(descriptionElement);
-sectionFiches.appendChild(stockElement);
+for (let i = 0; i < pieces.length; i++) {
+    // Création des balises 
+    const imageElement = document.createElement("img");
+    imageElement.src = pieces[i].image;
+    const nomElement = document.createElement("h2");
+    nomElement.innerText = pieces[i].nom;
+    const prixElement = document.createElement("p");
+    prixElement.innerText = `Prix: ${pieces[i].prix} € (${pieces[i].prix < 35 ? "€" : "€€€"})`;
+    const categorieElement = document.createElement("p");
+    categorieElement.innerText = pieces[i].categorie ?? "(aucune catégorie)";
+
+    //Création des élements pour l'exercice
+    const descriptionElement = document.createElement("p");
+    descriptionElement.innerText = pieces[i].description ?? "Pas de description pour le moment.";
+
+    const stockElement = document.createElement("p");
+    stockElement.innerText = pieces[i].disponibilite ? "En stock" : "Rupture de stock";
+
+    //Rattachement de nos balises au DOM
+    sectionFiches.appendChild(imageElement);
+    sectionFiches.appendChild(nomElement);
+    sectionFiches.appendChild(prixElement);
+    sectionFiches.appendChild(categorieElement);
+
+    //Ajout des éléments au DOM pour l'exercice
+    sectionFiches.appendChild(descriptionElement);
+    sectionFiches.appendChild(stockElement);
+}
